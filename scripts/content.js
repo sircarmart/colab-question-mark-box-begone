f732db7c263b04a14d55ee7717cfb326523f3cd7
sircarmart <carmartu7@gmail.com>
2024-09-29 23:17:40 -0700

    works on all colab notebook R pages and refactored code

--- scripts/content.js
+++ scripts/content.js
@@ -1,33 +1,38 @@
-// dictates how matching strings using regex is replaced
-function blankSpaceReplacer(match, p1, p2) {
-  return p2;
-};
+function waitForElements(selector) {
+  return new Promise(resolve => {
+    if (document.querySelectorAll(selector).length > 0) {
+      console.log("hit1")
+      return resolve(document.querySelectorAll(selector));
+    }
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelectorAll(selector).length > 0) {
+        console.log("hit2")
+        observer.disconnect();
+        resolve(document.querySelectorAll(selector));
+      }
+    });
 
-// Callback function to execute when mutations are observed
-const observerCallback = (mutationList, observer) => {
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
+
+const observer = new MutationObserver(mutationList => {
   for (const mutation of mutationList) { // go through all the mutations
     if (mutation.type === "childList" && ( mutation.addedNodes[0]?.matches?.("colab-tab") ?? false )) { // if the mutation type refers to a childList change
         if ( mutation.addedNodes[0]?.matches?.("colab-tab") ?? false ) {
-            console.log("A child node has been added or removed.");
-            console.log(mutation.addedNodes[0].children[0].children[0].children[0].textContent);
-            mutation.addedNodes[0].children[0].children[0].children[0].textContent = mutation.addedNodes[0].children[0].children[0].children[0].textContent.replace(/(_)(\w)/g, blankSpaceReplacer);
+          mutation.addedNodes[0].children[0].children[0].children[0].textContent = mutation.addedNodes[0].children[0].children[0].children[0].textContent.replace(/(_)/g, '');
         }
     } else if (mutation.type === "attributes") { // if the mutation type refers to the atributes of the observed element changing 
-    //   console.log(`The ${mutation.attributeName} attribute was modified.`);
     }
   }
-};
+});
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(observerCallback);
-
-// Select the node that will be observed for mutations
-
-const targetNodes = document.querySelectorAll("div.tab-pane-container").forEach((i) => {
-    if (i) {
-        observer.observe(i, config) // Start observing the target node for configured mutations
-    }
+waitForElements("div.tab-pane-container").then(elmList => {
+  elmList.forEach(elm => {
+    const targetNodes = observer.observe(elm, { childList: true, subtree: true})
+  });
 });
